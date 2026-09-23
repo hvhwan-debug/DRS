ad290abeafa2e64e600bba84356d2e7382db1cf6
hvhwan-debug <hv.hwan@gmail.com>
2026-09-23 23:19:23 +0700

    Trả về loại đánh giá thay vì môn học

--- api/admin-list-grades/index.js
+++ api/admin-list-grades/index.js
@@ -17,7 +17,7 @@ module.exports = async function (context, req) {
         parentEmail: entity.partitionKey,
         studentName: entity.studentName,
         program: entity.program,
-        subject: entity.subject,
+        assessmentType: entity.assessmentType || "Buổi học",
         term: entity.term,
         score: entity.score !== undefined && entity.score !== null ? entity.score : null,
         comment: entity.comment || "",
