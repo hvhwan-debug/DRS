76d429fee038cd64324def8ea3670f02137dc4ab
hvhwan-debug <hv.hwan@gmail.com>
2026-09-23 22:49:10 +0700

    Thêm helper mã hoá mật khẩu (scrypt, không cần thư viện ngoài)

---
+++ api/_shared/password.js
@@ -0,0 +1,17 @@
+const crypto = require("crypto");
+
+function hashPassword(password) {
+  const salt = crypto.randomBytes(16).toString("hex");
+  const hash = crypto.scryptSync(password, salt, 64).toString("hex");
+  return { salt, hash };
+}
+
+function verifyPassword(password, salt, expectedHash) {
+  const hash = crypto.scryptSync(password, salt, 64).toString("hex");
+  const a = Buffer.from(hash, "hex");
+  const b = Buffer.from(expectedHash, "hex");
+  if (a.length !== b.length) return false;
+  return crypto.timingSafeEqual(a, b);
+}
+
+module.exports = { hashPassword, verifyPassword };
